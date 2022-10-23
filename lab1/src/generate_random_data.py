816f871a1a766c3624d824ef001176e1ced8028d
drukhary <rkrisanoff@gmail.com>
2022-10-24 01:54:03 +0300

    update generating of data & appended new flag to the script

--- lab1/src/generate_random_data.py
+++ lab1/src/generate_random_data.py
@@ -15,70 +15,89 @@ def insertCommand(table, **kwargs):
 
 
 names = [
-    ["John", "Ivan", "Gans", "Li","Spartak","Nikita"],
-    ["Anna", "Kate", "Nastya", "Ekaterina","Youlya","Maria"]
-    ]
+    ["John", "Ivan", "Gans", "Li", "Spartak", "Nikita"],
+    ["Anna", "Kate", "Nastya", "Ekaterina", "Youlya", "Maria"]
+]
+
 games = [
     ("Hiding games", True),
     ("Chinese whispers", False)
 ]
+GAMES_NAME = 0
+GAMES_COMPENTITIVE = 1
+game_instances = list(range(10))
+
+questions = [
+    "WHO I AM?","WHERE I AM?","FOR WHAR I AM?","WHY I AM SO UNHAPPY?","HOW ADULTS HAVE GROWN OUT OF THESE WONDERFUL CREATURES?"
+]
 records = {
-    "E_CHILD": [],
-    "E_ADULT": [],
-    "E_GAME": [],
-    "E_GAME_INSTANCE": [],
-    "E_GAME_PARTICIPATION": [],
-    "E_GAME_WATCHING": [],
-    "E_FRIENDSHIP": []
+    "CHILD": [],
+    "ADULT": [],
+    "GAME": [],
+    "GAME_INSTANCE": [],
+    "GAME_PARTICIPATION": [],
+    "GAME_WATCHING": [],
+    "FRIENDSHIP": [],
+    "QUESTION": []
 }
 for i in range(len(names[0])):
     gender = rnd.randint(0, 1)
     name = names[gender][rnd.randint(0, len(names[gender]))-1]
     age = rnd.randint(0, 18)
-    records["E_CHILD"].append(insertCommand(
-        table="E_CHILD", id=i, name=name, age=age, gender=bool(gender)))
+    records["CHILD"].append(insertCommand(
+        table="CHILD", id=i, name=name, age=age, gender=bool(gender)))
 
-for i in range(len(names[0])):
+for index in range(len(names[0])):
     gender = rnd.randint(0, 1)
     name = names[gender][rnd.randint(0, len(names[gender]))-1]
     age = rnd.randint(18, 50)
-    is_accept_as_friend=rnd.randint(0, 1)
-    records["E_ADULT"].append(insertCommand(
-        table="E_ADULT", id=i, name=name, age=age, gender=bool(gender)))
+    is_accept_as_friend = rnd.randint(0, 1)
+    records["ADULT"].append(insertCommand(
+        table="ADULT", id=index, name=name, age=age, gender=bool(gender)))
 
-for name, is_competitive in games:
-    records["E_GAME"].append(insertCommand(
-        table="E_GAME", name=name, is_competitive=is_competitive))
-game_instances = list(range(10))
+for index, (name, is_competitive) in enumerate(games):
+    records["GAME"].append(insertCommand(
+        table="GAME", id=index, name=name, is_competitive=is_competitive))
+
+
 for i in game_instances:
     game = rnd.randint(0, len(games)-1)
-    records["E_GAME_INSTANCE"].append(insertCommand(
-        table="E_GAME_INSTANCE",
+    records["GAME_INSTANCE"].append(insertCommand(
+        table="GAME_INSTANCE",
         id=i,
-        name_of_the_game=games[game][0],
+        game_id=rnd.randint(0,len(games)-1),
         won_child_id=rnd.randint(
-            0, len(names[0])-1) if games[game][1] else "NULL"
+            0, len(names[0])-1) if games[game][GAMES_COMPENTITIVE] else "NULL"
     ))
 for i in range(10):
     game = rnd.randint(0, len(games)-1)
-    records["E_GAME_PARTICIPATION"].append(insertCommand(
-        table="E_GAME_PARTICIPATION",
+    records["GAME_PARTICIPATION"].append(insertCommand(
+        table="GAME_PARTICIPATION",
         child_id=rnd.randint(0, len(names)-1),
         game_instance_id=rnd.randint(0, len(game_instances)-1)
     ))
 for i in range(10):
-    game = rnd.randint(0, len(games))
-    records["E_GAME_WATCHING"].append(insertCommand(
-        table="E_GAME_WATCHING",
+    game = rnd.randint(0, len(games)-1)
+    records["GAME_WATCHING"].append(insertCommand(
+        table="GAME_WATCHING",
         adult_id=rnd.randint(0, len(names)-1),
         game_instance_id=rnd.randint(0, len(game_instances)-1)
     ))
 
 for i in range(10):
-    records["E_FRIENDSHIP"].append(insertCommand(
-        table="E_FRIENDSHIP",
+    records["FRIENDSHIP"].append(insertCommand(
+        table="FRIENDSHIP",
         child_id=rnd.randint(0, len(names[0])-1),
-        adult_id=rnd.randint(0, len(names[0])-1)
+        adult_id=rnd.randint(0, len(names[0])-1),
+        power=rnd.randint(0,100)
+    ))
+
+for i in range(5):
+    records["QUESTION"].append(insertCommand(
+        table="QUESTION",
+        adult_id=rnd.randint(0, len(names[0])-1),
+        content=questions[rnd.randint(0,len(questions)-1)],
+        priority=rnd.randint(0,100)
     ))
 
 
