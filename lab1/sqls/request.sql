b659bc3bd8f9ad62de43a68cbc30d23986835782
drukhary <rkrisanoff@gmail.com>
2022-10-28 00:12:43 +0300

    simplified request

--- lab1/sqls/request.sql
+++ lab1/sqls/request.sql
@@ -1,32 +1,31 @@
-SELECT 
-    game_instance.id AS game_instance_id,
+SELECT
     game.id AS game_type_id,
     game.name AS game_name,
+    game_instance.id AS game_instance_id,
     child.id AS child_id,
     child.name AS child_name
-FROM (
+FROM
+    (
         child
         JOIN friendship ON child.id = friendship.child_id
         JOIN game_participation ON friendship.child_id = game_participation.child_id
         JOIN game_instance ON game_participation.game_instance_id = game_instance.id
         JOIN game ON game_instance.game_id = game.id
     )
-GROUP BY 
+WHERE
+    child.gender = 'male'
+GROUP BY
     child.id,
     friendship.child_id,
     game_participation.child_id,
     game_instance.id,
     game.id,
     friendship.power
-HAVING (
-        child.gender = 'male'
-        AND (
-            (count(*) > 5)
-            OR (
-                count(*) <= 5
-                AND friendship.power = 99
-            )
+HAVING
+    (
+        (count(*) > 5)
+        OR (
+            count(*) <= 5
+            AND friendship.power = 99
         )
-    )
-    
-LIMIT 2;
+    );
