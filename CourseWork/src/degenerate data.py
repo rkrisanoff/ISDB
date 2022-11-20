8192ffa49c370f56b326a08376b2e0f9d458a2df
drukhary <rkrisanoff@gmail.com>
2022-11-20 20:08:40 +0300

    added domain that incapsulated area of allowing values

--- CourseWork/src/degenerate data.py
+++ CourseWork/src/degenerate data.py
@@ -9,36 +9,53 @@ def randomword(length):
     letters = string.ascii_lowercase
     return ''.join(rnd.choice(letters) for i in range(length))
 
+
 class FieldTypeDefinition(NamedTuple):
     name: str
     """
     name is unique value that detemrnies field in entity
     """
-    generate: Callable[['any'], 'any']
-    """
-    generate - function returning value from domen for this field type
-    """
     toSql: Callable[['any'], 'str']
     """
     To Sql - translate python-value to value in SQL
     """
-    
+
+
+NamesDomain = [
+    'Harry',
+    'Oliver',
+    'Jack',
+    'Charlie',
+    'Thomas',
+    'Jacob',
+    'Alfie',
+    'Riley',
+    'William',
+    'James'
+]
+GenderDomain = ['male', 'gender']
+
+
+class Domain(Enum):
+    def GENDER(): return rnd.choice(GenderDomain)
+    def NAME(): return rnd.choice(NamesDomain)
+    def AGE(): return rnd.randint(18, 60)
+    def FLAG(): return rnd.random() >= 0.5
+    def INDEX(): return rnd.randint(1, 1000)
+    def POSITIVE_INTEGER(): return rnd.randint(0,20000)
 
 
 class FieldType(FieldTypeDefinition, Enum):
     INTEGER = FieldTypeDefinition(
         name="INTEGER",
-        generate=lambda min, max: rnd.randint(min, max),
         toSql=lambda value: str(value)
     )
     VARCHAR = FieldTypeDefinition(
         name="VARCHAR",
-        generate=lambda len: randomword(len),
         toSql=lambda value: str("'"+value+"'")
     )
     BOOLEAN = FieldTypeDefinition(
         name="BOOLEAN",
-        generate=lambda: rnd.random() > 0.5,
         toSql=lambda value: "TRUE" if value else "NOT"
     )
 
@@ -56,13 +73,18 @@ class Field:
     flag whether the value is a NULL
     """
     FK: dict[str]
+    domain: Domain
 
-    def __init__(self, name: str, type: FieldType, isPK: bool, mayBeNull: bool, condition, FK: dict[str, (str, str)] = dict()) -> None:
+    def __init__(self, name: str, type: FieldType, domain: Domain, isPK: bool, mayBeNull: bool, condition, FK: dict[str, (str, str)] = dict()) -> None:
         self.type = type
         self.condition = condition
         self.name = name
         self.isPK = isPK
         self.mayBeNull = mayBeNull
+        self.domain = domain
+
+    def getRandomValueFromDomain(self):
+        return self.domain()
 
 
 class Entity:
@@ -87,11 +109,8 @@ class EntityFactory:
         values = dict().fromkeys(entity.fields.keys())
         for field in list(entity.fields.values()):
             value = None
-            if field.type == FieldType.INTEGER:
-                while not (value := field.type.generate(1, 1000)):
-                    value = field.type.generate(1, 1000)
-            elif field.type == FieldType.VARCHAR:
-                value = field.type.generate(30)
+            while not (value := field.getRandomValueFromDomain()):
+                value = field.getRandomValueFromDomain()
             values[field.name] = field.type.toSql(value)
         return values
 
@@ -100,34 +119,47 @@ class EntityFactory:
 
     def generateSqls(self, entities: dict[str, Entity]):
         sqls = dict().fromkeys(entities.keys())
-        for entityName,entity in entities.items():
-            sqls[entityName] = self.generateInserts(entity,10)
+        for entityName, entity in entities.items():
+            sqls[entityName] = self.generateInserts(entity, 10)
         finalRequst = ""
-        for entityName,inserts in sqls.items(): 
+        for entityName, inserts in sqls.items():
             for insert in inserts:
-                finalRequst += self.transormToSql(entityName,insert)
+                finalRequst += self.transormToSql(entityName, insert)
         return finalRequst
 
 
 ef = EntityFactory({"schema": "s284712"})
 positronic_brain = Entity("positronic_brain",
-           {
-               "release_series": Field("release_series", FieldType.INTEGER, True, False, lambda val: True, {}),
-               "name": Field("name", FieldType.VARCHAR, False, False, lambda val: len(val) <= 255, {}),
-               "speed": Field("speed", FieldType.INTEGER, False, False, lambda val: val > 0, {}),
-               "cost": Field("cost", FieldType.INTEGER, False, False, lambda val: val > 0, {})
-           }
-           )
+                          {
+                              "release_series":
+                              Field("release_series", FieldType.INTEGER,
+                                    Domain.INDEX, True, False, lambda val: True, {}),
+                              "name":
+                              Field("name", FieldType.VARCHAR, Domain.NAME,
+                                    False, False, lambda val: len(val) <= 255, {}),
+                              "speed":
+                              Field("speed", FieldType.INTEGER, Domain.INDEX,
+                                    False, False, lambda val: val > 0, {}),
+                              "cost":
+                              Field("cost", FieldType.INTEGER, Domain.INDEX,
+                                    False, False, lambda val: val > 0, {})
+                          }
+                          )
 department = Entity("department",
-           {
-               "id": Field("id", FieldType.INTEGER, True, False, lambda val: True, {}),
-               "extracted_bor_quantity": Field("extracted_bor_quantity", FieldType.INTEGER, False, False, lambda val: val >= 0, {}),
-               "current_resource": Field("current_resource", FieldType.INTEGER, False, False, lambda val: val >= 0, {})
-           }
-           )
+                    {
+                        "id":
+                        Field("id", FieldType.INTEGER, Domain.INDEX,
+                              True, False, lambda val: True, {}),
+                        "extracted_bor_quantity":
+                        Field("extracted_bor_quantity", FieldType.INTEGER,
+                              Domain.INDEX, False, False, lambda val: val >= 0, {}),
+                        "current_resource":
+                        Field("current_resource", FieldType.INTEGER,
+                              Domain.INDEX, False, False, lambda val: val >= 0, {})
+                    }
+                    )
 entities = {
-    "positronic_brain":positronic_brain,
-    "department":department
+    "positronic_brain": positronic_brain,
+    "department": department
 }
 print(ef.generateSqls(entities))
-
